c807948f818c2dff05949aaf6a795cc7b5db04c5
LIJI678 <102945984+LIJI678@users.noreply.github.com>
2022-06-13 00:58:08 +0530

    Add files via upload

---
+++ alumini.php
@@ -0,0 +1,311 @@
+<?php   ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>DCS CUSAT</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link rel="stylesheet" href="style.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+	  <div class="bg-top navbar-light">
+    	<div class="container">
+    		<div class="row no-gutters d-flex align-items-center align-items-stretch">
+    			<div class="col-md-4 d-flex align-items-center py-4">
+					<img src="image/logo4.png" height="110px" width="100px">
+    				<a class="navbar-brand" href="home.php">DCS,CUSAT<span></span></a>
+    			</div>
+	    		<div class="col-lg-8 d-block">
+		    		<div class="row d-flex">
+					    <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
+					    	<div class="icon d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
+					    	<div class="text">
+					    		<span>Email</span>
+						    	<span>csdir@cusat.ac.in</span>
+						    </div>
+					    </div>
+					    <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
+					    	<div class="icon d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+						    <div class="text">
+						    	<span>Call</span>
+						    	<span>Call Us: 0484-2862301, 0484-2577126</span>
+						    </div>
+					    </div>
+					    <div class="col-md topper d-flex align-items-center justify-content-end">
+					    	<p class="mb-0">
+					    		<a href="login.php" class="btn py-2 px-3 btn-primary d-flex align-items-center justify-content-center">
+					    			<span>Log In</span>
+					    		</a>
+					    	</p>
+					    </div>
+				    </div>
+			    </div>
+		    </div>
+		  </div>
+    </div>
+	  <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container d-flex align-items-center px-4">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+	      <form action="#" class="searchform order-lg-last">
+          <div class="form-group d-flex">
+            <input type="text" class="form-control pl-3" placeholder="Search">
+            <button type="submit" placeholder="" class="form-control search"><span class="ion-ios-search"></span></button>
+          </div>
+        </form>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav mr-auto">
+	        	<li class="nav-item"><a href="home.php" class="nav-link pl-0">Home</a></li>
+            <!--<li class="nav-item"><a href="academics" class="nav-link"> </a></li>-->
+            <div class="dropdown"><a href="academics" class="nav-link">
+              <button class="dropbtn nav-item active">Academics 
+                <i class="fa fa-caret-down"></i>
+              </button>
+                <div class="dropdown-content">
+                  <a href="Elearning.php">E-Learning</a>
+                  <a href="courses.php">Courses</a>
+                  <a href="Research.php">Research</a>
+                </div>
+            </div> 
+           
+	        	<!--<li class="nav-item"><a href="academics" class="nav-link">Academics</a></li>
+	        	<li class="nav-item"><a href="courses.php" class="nav-link">Courses</a></li>-->
+            <div class="dropdown"><a href="#" class="nav-link">
+              <button class="dropbtn nav-item active">Facilities 
+                <i class="fa fa-caret-down"></i>
+              </button>
+                <div class="dropdown-content">
+                  <a href="Lab.php">Lab</a>
+                  <a href="Library.php">Library</a>
+                  <a href="ccenter.php">Computer Centre</a>
+                  <a href="http://dspace.cusat.ac.in/jspui/" target="_blank">Digital Library</a>
+                </div>
+            </div> 
+	        	<li class="nav-item"><a href="teachers.php" class="nav-link">Staff</a></li>
+	        	
+            <div class="dropdown"><a href="Others" class="nav-link">
+              <button class="dropbtn nav-item active">Others 
+                <i class="fa fa-caret-down"></i>
+              </button>
+                <div class="dropdown-content">
+                  <a href="placement">Placements</a>
+                  <a href="alumni">Alumni</a>
+                  <a href="gallery.php">Photo Gallery</a>
+                </div>
+            </div> 
+	          <!--<li class="nav-item"><a href="contact.html" class="nav-link">Contact Us</a></li>-->
+            <li class="nav-item"><a href="https://admissions.cusat.ac.in/" target="_blank" class="nav-link">Admissions</a></li>
+			  <li class="nav-item"><a href="https://www.cusat.ac.in/dcs.php" target="_blank" class="nav-link">Old Website</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+	
+   <section class="ftco-section ftco-no-pt ftc-no-pb">
+			<div class="container"> 
+	
+	
+	<table width="90%" border="0" align="center">
+          <tr>
+            <td class='csstyle2'>We are in the process of collecting 
+              and updating the alumni details of the Department 
+              of Computer Science, CUSAT. By doing so we hope to 
+              provide a meeting place for all previous and current 
+              students of the department whereby they can know each 
+              other better and share their knowledge. Also upon 
+              getting in touch with enough alumnus we intend to 
+              create a strong and active <strong>alumni asscociation</strong> for the department.
+              
+      <p>If you were a student of this department, please send a mail to <strong>dcsalumni@cusat.ac.in</strong> 
+        giving your email id(will be used for login), name, batch, programme, 
+        present address and phone no., current profile etc. Please send a scanned 
+        passport size photo(preferably color) of yours as well.Your details including 
+        your photo will be accesible only to the alumnus upon login and not to 
+        the public. <br />
+                <br />
+                To get more infomation about Alumni members, Please use<strong> <a href="login.jsp">Alumni Login</a> </strong>page.<strong><br />
+                 <br />
+                  </strong>New Members, Please use <strong><strong><a href="joinalumni.jsp">Join Alumni</a></strong>  </strong>page.<strong><br />
+                  </strong></p></td>
+          </tr>
+        </table>
+		
+</section >
+			</div >
+
+   <section class="ftco-section ftco-no-pt ftc-no-pb">
+			<div class="container">
+			
+			
+				
+<table width="90%" border="0" align="center" cellpadding="0" cellspacing="0" >
+  <tr> 
+       
+<form action="/">
+  <label for="batch">Choose a Batch:</label>
+  <select style="background-color:#fd5f00; color:#fff "  name="batch" id="batch">
+    <option value="2021-2023">2021-2023</option>
+    <option value="2021-2023">2020-2022</option>
+    <option value="2021-2023">2019-2021</option>
+    <option value="2021-2023">2018-2020</option>
+  </select>
+  <br><br>
+  <input type="submit" value="Submit">
+</form>
+</tr>
+     <!--   <tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2019'>2019-2021</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2018'>2018-2020</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2017'>2017-2019</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2016'>2016-2018</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2015'>2015-2017</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2014'>2014-2016</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2013'>2013-2015</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2012'>2012-2014</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2011'>2011-2013</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2010'>2010-2012</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2009'>2009-2011</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2008'>2008-2010</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2007'>2007-2009</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2006'>2006-2008</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2005'>2005-2007</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2004'>2004-2006</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2003'>2003-2005</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2002'>2002-2004</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2001'>2001-2003</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=2000'>2000-2002</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=1999'>1999-2001</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=1998'>1998-2000</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=1997'>1997-1999</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=1996'>1996-1998</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=1995'>1995-1997</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=1994'>1994-1996</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=1993'>1993-1995</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=1992'>1992-1994</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=1991'>1991-1993</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=1990'>1990-1992</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=1989'>1989-1991</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=1988'>1988-1990</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=1987'>1987-1989</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=1986'>1986-1988</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=1985'>1985-1987</a></div></th></tr>
+<tr><th height='20' bgcolor='#FFFFFF' class='csstyle4' scope='col'><div align='center'><a href='#=1984'>1984-1986</a></div></th></tr>-->
+
+      </table></th>
+    <th width="81%" valign="top" scope="col"> <form action="alumni.jsp" method="post" name="sfrm" id="sfrm">
+        <table width="56%" border="0" align="center" cellpadding="0" cellspacing="0">
+          <tr> 
+            <th width="34%" scope="col"><span class="csstyle4">Search Name</span></th>
+            <th width="23%" scope="col"><font face="arial,helvetica" 
+                                size="1"> 
+              <input name="txtsearch" type="text" id="txtsearch" size="15" maxlength="30" style=" BORDER-RIGHT: 1px solid; BORDER-TOP: 1px solid; BORDER-LEFT: 1px solid; COLOR: #999999; BORDER-BOTTOM: 1px solid; " />
+              </font></th>
+            <th width="43%" scope="col"><div align="left"><font face="arial,helvetica" 
+                                size="1"> 
+                <input name="image" type="image" src="images/go_button.gif" width="28" height="21" border="0">
+                </font></div></th>
+          </tr>
+        </table>
+      </form>
+      <table width="99%" height="0" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+        <tr> 
+          <th width="26%" height="18" bgcolor="#FFFFFF" class="csstyle4" scope="col"><div align="center">Name</div></th>
+          <th width="21%" height="18" bgcolor="#FFFFFF" class="csstyle4" scope="col"><div align="center">Course/Batch</div></th>
+          <th width="53%" height="18" bgcolor="#FFFFFF" class="csstyle4" scope="col"><div align="center">Current 
+          Profile</div></th>
+        </tr>
+        
+      </table></th>
+  </tr>
+  
+</table>
+		<br>
+                      <br>	
+			
+</section >
+			</div >
+<!-- content ends here  -->
+
+<footer class="ftco-footer ftco-bg-dark ftco-section">
+        <div class="container">
+          <div class="row mb-0">
+            <div class="col-md-4 col-lg-3">
+              <div class="ftco-footer-widget mb-0">
+                  <h2 class="ftco-heading-2">Have Questions?</h2>
+                  <div class="block-23 mb-0">
+                    <ul>
+                      <li><span class="icon icon-map-marker"></span><span class="text">CUSAT Campus, Kalamassery, Cochin</span></li>
+                      <li><a href="#"><span class="icon icon-phone"></span><span class="text">0484-2577126, 0484-2862301</span></a></li>
+                      <li><a href="#"><span class="icon icon-envelope"></span><span class="text">csdir@cusat.ac.in</span></a></li>
+                    </ul>
+                  </div>
+              </div>
+            
+            
+            </div>
+            <div class="col-md-4 col-lg-3">
+              <div class="ftco-footer-widget mb-0 mr-md-4">
+                <h2 class="ftco-heading-2">Links</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Home</a></li>
+                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Academics</a></li>
+                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Courses</a></li>
+                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>People</a></li>
+                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Admission</a></li>
+                </ul>
+              </div>
+            </div>
+            
+              <div class="ftco-footer-widget text-center mb-0">
+              <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved</a>
+    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+            </div>
+          </div>
+        </div>
+      </footer>
+      
+    
+  
+    <!-- loader -->
+    <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+  
+  
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/scrollax.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+    <script src="js/google-map.js"></script>
+    <script src="js/main.js"></script>
+      
+  </body>
+</html>
