80bf755e703be9086e4f100451c3f0cf1cae0af8
LIJI678 <102945984+LIJI678@users.noreply.github.com>
2022-06-13 01:19:51 +0530

    Add files via upload

---
+++ aicte.php
@@ -0,0 +1,205 @@
+<?php   ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>DCS CUSAT</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link rel="stylesheet" href="style.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+	  <div class="bg-top navbar-light">
+    	<div class="container">
+    		<div class="row no-gutters d-flex align-items-center align-items-stretch">
+    			<div class="col-md-4 d-flex align-items-center py-4">
+					<img src="image/logo4.png" height="110px" width="100px">
+    				<a class="navbar-brand" href="home.php">DCS,CUSAT<span></span></a>
+    			</div>
+	    		<div class="col-lg-8 d-block">
+		    		<div class="row d-flex">
+					    <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
+					    	<div class="icon d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
+					    	<div class="text">
+					    		<span>Email</span>
+						    	<span>csdir@cusat.ac.in</span>
+						    </div>
+					    </div>
+					    <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
+					    	<div class="icon d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+						    <div class="text">
+						    	<span>Call</span>
+						    	<span>Call Us: 0484-2862301, 0484-2577126</span>
+						    </div>
+					    </div>
+					    <div class="col-md topper d-flex align-items-center justify-content-end">
+					    	<p class="mb-0">
+					    		<a href="login.php" class="btn py-2 px-3 btn-primary d-flex align-items-center justify-content-center">
+					    			<span>Log In</span>
+					    		</a>
+					    	</p>
+					    </div>
+				    </div>
+			    </div>
+		    </div>
+		  </div>
+    </div>
+	  <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container d-flex align-items-center px-4">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+	      <form action="#" class="searchform order-lg-last">
+          <div class="form-group d-flex">
+            <input type="text" class="form-control pl-3" placeholder="Search">
+            <button type="submit" placeholder="" class="form-control search"><span class="ion-ios-search"></span></button>
+          </div>
+        </form>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav mr-auto">
+	        	<li class="nav-item"><a href="home.php" class="nav-link pl-0">Home</a></li>
+            <!--<li class="nav-item"><a href="academics" class="nav-link"> </a></li>-->
+            <div class="dropdown"><a href="academics" class="nav-link">
+              <button class="dropbtn nav-item active">Academics 
+                <i class="fa fa-caret-down"></i>
+              </button>
+                <div class="dropdown-content">
+                  <a href="Elearning.php">E-Learning</a>
+                  <a href="courses.php">Courses</a>
+                  <a href="Research.php">Research</a>
+                </div>
+            </div> 
+           
+	        	<!--<li class="nav-item"><a href="academics" class="nav-link">Academics</a></li>
+	        	<li class="nav-item"><a href="courses.php" class="nav-link">Courses</a></li>-->
+            <div class="dropdown"><a href="#" class="nav-link">
+              <button class="dropbtn nav-item active">Facilities 
+                <i class="fa fa-caret-down"></i>
+              </button>
+                <div class="dropdown-content">
+                  <a href="Lab.php">Lab</a>
+                  <a href="Library.php">Library</a>
+                  <a href="ccenter.php">Computer Centre</a>
+                  <a href="http://dspace.cusat.ac.in/jspui/" target="_blank">Digital Library</a>
+                </div>
+            </div> 
+	        	<li class="nav-item"><a href="teachers.php" class="nav-link">Staff</a></li>
+	        	
+            <div class="dropdown"><a href="Others" class="nav-link">
+              <button class="dropbtn nav-item active">Others 
+                <i class="fa fa-caret-down"></i>
+              </button>
+                <div class="dropdown-content">
+                  <a href="placement">Placements</a>
+                  <a href="alumni">Alumni</a>
+                  <a href="gallery.php">Photo Gallery</a>
+                </div>
+            </div> 
+	          <!--<li class="nav-item"><a href="contact.html" class="nav-link">Contact Us</a></li>-->
+            <li class="nav-item"><a href="https://admissions.cusat.ac.in/" target="_blank" class="nav-link">Admissions</a></li>
+			  <li class="nav-item"><a href="https://www.cusat.ac.in/dcs.php" target="_blank" class="nav-link">Old Website</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+	
+   <section class="ftco-section ftco-no-pt ftc-no-pb">
+			<div class="container">
+				<div class="row d-flex">
+					<div class="col-md-5 order-md-last wrap-about wrap-about d-flex align-items-stretch">
+						<div class="img" style="background-image: url(images/aicte.jpg); border"></div>
+					</div>
+					<div class="col-md-7 wrap-about py-5 pr-md-4 ftco-animate "><h3 class="mb-4" style="background-color: #fd5f00; color:#fff">AICTE Approval</h3>
+						<p  > <a style="font-weight: bold; color: #212529;"href="#url"> Letter of Approval 2017-18</a></p>
+						<p  ><a style="font-weight: bold; color: #212529;"href="#url"> Approval 2018-19</a></p>
+						<p  > <a style="font-weight: bold; color: #212529;" href="#url"> Approval 2019-20</a></p>
+						<p  > <a style="font-weight: bold; color: #212529;" href="#url"> Approval 2020-21</a></p>
+						<p  > <a style="font-weight: bold; color: #212529;" href="#url"> Approval 2021-22</a></p>
+						<p  > <a style="font-weight: bold; color: #212529;" href="#url">Mandatory disclosure</a></p>	
+					</div>
+</section >
+			</div >
+<!-- content ends here  -->
+
+<footer class="ftco-footer ftco-bg-dark ftco-section">
+        <div class="container">
+          <div class="row mb-0">
+            <div class="col-md-4 col-lg-3">
+              <div class="ftco-footer-widget mb-0">
+                  <h2 class="ftco-heading-2">Have Questions?</h2>
+                  <div class="block-23 mb-0">
+                    <ul>
+                      <li><span class="icon icon-map-marker"></span><span class="text">CUSAT Campus, Kalamassery, Cochin</span></li>
+                      <li><a href="#"><span class="icon icon-phone"></span><span class="text">0484-2577126, 0484-2862301</span></a></li>
+                      <li><a href="#"><span class="icon icon-envelope"></span><span class="text">csdir@cusat.ac.in</span></a></li>
+                    </ul>
+                  </div>
+              </div>
+            
+            
+            </div>
+            <div class="col-md-4 col-lg-3">
+              <div class="ftco-footer-widget mb-0 mr-md-4">
+                <h2 class="ftco-heading-2">Links</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Home</a></li>
+                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Academics</a></li>
+                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Courses</a></li>
+                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>People</a></li>
+                  <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Admission</a></li>
+                </ul>
+              </div>
+            </div>
+            
+              <div class="ftco-footer-widget text-center mb-0">
+              <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved</a>
+    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+            </div>
+          </div>
+        </div>
+      </footer>
+      
+    
+  
+    <!-- loader -->
+    <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+  
+  
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/scrollax.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+    <script src="js/google-map.js"></script>
+    <script src="js/main.js"></script>
+      
+  </body>
+</html>
